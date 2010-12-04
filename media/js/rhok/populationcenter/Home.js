59f2ad4eda7dd0ae268522fc8356b7e142409d2e
Charles Chan <chancharles@gmail.com>
2010-12-04 15:10:42 -0500

    add query by coordinates

--- media/js/rhok/populationcenter/Home.js
+++ media/js/rhok/populationcenter/Home.js
@@ -1,15 +1,25 @@
 rhok.populationcenter.Home = function(settings) {
-	
+
 	var self = this;
 	self.settings = settings;
 
-	
 	self.init = function(settings) {
-		var html = rhok.populationcenter.soy.pages.home({});
+		var html = rhok.populationcenter.soy.pages.home( {});
 		settings.container.html(html);
-		
-		
-		
+
+		self.$querySection = $(".query-section");
+		self.$guessMyLocation = $(".guess-my-location", self.$querySection);
+		self.$coordinatesInput = $("input[name='coordinates']",
+				self.$querySection);
+
+		self.$guessMyLocation.click(function() {
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(function(position) {
+					self.$coordinatesInput.val(position.coords.latitude + ","
+							+ position.coords.longitude);
+				});
+			}
+		});
 	};
-	
+
 };
