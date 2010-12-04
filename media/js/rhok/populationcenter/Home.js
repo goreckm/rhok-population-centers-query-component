8462b2311a37d3130f9a79ab9297062697b10926
Charles Chan <chancharles@gmail.com>
2010-12-04 16:21:13 -0500

    add map

--- media/js/rhok/populationcenter/Home.js
+++ media/js/rhok/populationcenter/Home.js
@@ -11,16 +11,70 @@ rhok.populationcenter.Home = function(settings) {
 		self.$guessMyLocation = $(".guess-my-location", self.$querySection);
 		self.$coordinatesInput = $("input[name='coordinates']",
 				self.$querySection);
+		self.$populationCentersMap = $("#population-centers-map");
+
+		var myOptions = {
+			zoom : 6,
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document
+				.getElementById("population-centers-map"), myOptions);
+		var browserSupportFlag = false;
 
 		self.$guessMyLocation.click(function() {
+			var coordinates = undefined;
 			if (navigator.geolocation) {
 				navigator.geolocation.getCurrentPosition(function(position) {
-					self.$coordinatesInput.val(position.coords.latitude + ","
-							+ position.coords.longitude);
+					coordinates = {
+						latitude : position.coords.latitude,
+						longitude : position.coords.longitude
+					};
+					centerMap(coordinates);
+				}, function() {
+					handleNoGeolocation(browserSupportFlag);
+				});
+				// Try Google Gears Geolocation
+			} else if (google.gears) {
+				browserSupportFlag = true;
+				var geo = google.gears.factory.create('beta.geolocation');
+				geo.getCurrentPosition(function(position) {
+					coordinates = {
+						latitude : position.latitude,
+						longitude : position.longitude
+					};
+					centerMap(coordinates);
+				}, function() {
+					handleNoGeoLocation(browserSupportFlag);
+				});
+				// Browser doesn't support Geolocation
+			} else {
+				browserSupportFlag = false;
+				handleNoGeolocation(browserSupportFlag);
+			}
+
+			function handleNoGeolocation(errorFlag) {
+				if (errorFlag == true) {
+					alert("Geolocation service failed.");
+				} else {
+					alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+				}
+			}
+
+			function centerMap(center) {
+				var location = new google.maps.LatLng(center.latitude,
+						center.longitude);
+				map.setCenter(location);
+				var marker = new google.maps.Marker( {
+					position : location,
+					map : map,
+					animation : google.maps.Animation.DROP,
+					title : center.latitude + "," + center.longitude
 				});
 			}
+
 			return false;
 		});
+
 	};
 
 };
